a440912b4bd385dc2d91edbbcd8c974daf15aee6
Sontaya W <tonklasw@icloud.com>
2018-02-16 16:07:36 +0700

    fix
    fixed bug.

--- popup.js
+++ popup.js
@@ -1,21 +1,21 @@
 // DEV. EXTENTION BY iTON // => POPUP.JS
 // NOTE: Click to Shorten URL
 
-function setURLshorten(shtURL, title) {
+function setURLshorten(shtURL) {
   // console.log(shtURL);
   let input = document.getElementById("url");
   if (shtURL != undefined) {
     hide();
     input.value = shtURL;
     copy();
-    share(shtURL, title);
+    share(shtURL);
   }
 
 }
 
 function copy() {
   // console.log(document.getElementById("url").value);
-  let copyText = document.querySelector("#url");
+  var copyText = document.querySelector("#url");
   copyText.select();
   document.execCommand("copy");
 }
@@ -28,12 +28,11 @@ function hide() {
   complete.style.display = "inline";
 }
 
-function share(shtURL, title_o) {
-  let title = encodeURI(title_o)
-  let url = encodeURI(shtURL);
-  let tweet = document.getElementById("tweet");
-  let facebook = document.getElementById("facebook");
-  tweet.src = "https://platform.twitter.com/widgets/tweet_button.html?size=m&url=" + shtURL + "&related=twitterapi%2Ctwitter&text=" + title + "&hashtags=iShortener";
+function share(shtURL) {
+  var url = shtURL;
+  var tweet = document.getElementById("tweet");
+  var facebook = document.getElementById("facebook");
+  tweet.src = "https://platform.twitter.com/widgets/tweet_button.html?size=m&url=" + shtURL + "&related=twitterapi%2Ctwitter&text=share%20url&hashtags=iShortener";
   document.getElementsByTagName('iframe')[1].parentNode.appendChild(tweet);
 
   facebook.src = "https://www.facebook.com/plugins/share_button.php?href=" + shtURL + "&layout=button&size=small&mobile_iframe=true&width=60&height=20&appId";
