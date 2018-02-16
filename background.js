655fd4407915fd90a72a1c53e128ccafe3bd29e3
Sontaya W <tonklasw@icloud.com>
2018-02-16 16:05:51 +0700

    fix
    fixed bug.

--- background.js
+++ background.js
@@ -4,14 +4,16 @@
 const API_KEY = 'AIzaSyCjkUcGt5h2mFyNfgJFqUXqKY1kOQnjL4g';
 const API_URL = 'https://www.googleapis.com/urlshortener/v1/url';
 let TAB_URL = '';
+let TITLE = '';
 
 function onGot(page) {
   chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs) {
     TAB_URL = tabs[0].url;
-    // console.log('url: '+ TAB_URL);
+    TITLE = tabs[0].title;
+    // console.log('TITLE: '+ TITLE);
     // console.log(page);
     if (TAB_URL) {
-      shortenLink(TAB_URL);
+      shortenLink(TAB_URL, TITLE);
     }
   });
 }
@@ -23,7 +25,7 @@ function onError(error) {
   console.log(`Error: ${error}`);
 }
 
-function shortenLink(link) {
+function shortenLink(link, title) {
   const basename = "https://www.googleapis.com";
   const urlfrag = "/urlshortener/v1/url?key=" + API_KEY;
   const longUrl = encodeURIComponent(link);
@@ -34,7 +36,7 @@ function shortenLink(link) {
   xhr.onreadystatechange = function () {
     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
       const response = (JSON.parse(xhr.responseText));
-      setURLshorten(response.id);
+      setURLshorten(response.id, title);
     }
   };
 
--- popup.js
+++ popup.js
@@ -1,21 +1,21 @@
 // DEV. EXTENTION BY iTON // => POPUP.JS
 // NOTE: Click to Shorten URL
 
-function setURLshorten(shtURL) {
+function setURLshorten(shtURL, title) {
   // console.log(shtURL);
   let input = document.getElementById("url");
   if (shtURL != undefined) {
     hide();
     input.value = shtURL;
     copy();
-    share(shtURL);
+    share(shtURL, title);
   }
 
 }
 
 function copy() {
   // console.log(document.getElementById("url").value);
-  var copyText = document.querySelector("#url");
+  let copyText = document.querySelector("#url");
   copyText.select();
   document.execCommand("copy");
 }
@@ -28,11 +28,12 @@ function hide() {
   complete.style.display = "inline";
 }
 
-function share(shtURL) {
-  var url = shtURL;
-  var tweet = document.getElementById("tweet");
-  var facebook = document.getElementById("facebook");
-  tweet.src = "https://platform.twitter.com/widgets/tweet_button.html?size=m&url=" + shtURL + "&related=twitterapi%2Ctwitter&text=share%20url&hashtags=iShortener";
+function share(shtURL, title_o) {
+  let title = encodeURI(title_o)
+  let url = encodeURI(shtURL);
+  let tweet = document.getElementById("tweet");
+  let facebook = document.getElementById("facebook");
+  tweet.src = "https://platform.twitter.com/widgets/tweet_button.html?size=m&url=" + shtURL + "&related=twitterapi%2Ctwitter&text=" + title + "&hashtags=iShortener";
   document.getElementsByTagName('iframe')[1].parentNode.appendChild(tweet);
 
   facebook.src = "https://www.facebook.com/plugins/share_button.php?href=" + shtURL + "&layout=button&size=small&mobile_iframe=true&width=60&height=20&appId";
