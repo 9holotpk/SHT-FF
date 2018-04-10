60bce34ed2bb54053d1bb299a2f2a6ec2f5f6186
Sontaya W <tonklasw@icloud.com>
2018-04-10 13:31:10 +0700

    update
    - update API

--- background.js
+++ background.js
@@ -1,8 +1,15 @@
 // DEV. EXTENTION BY iTON // => BACKGROUND.JS
-// NOTE: Click to Shorten URL
+// NOTE:    Click to Shorten URL
+// UPDATE:  10/04/2018 - Transitioning Google URL Shortener to Firebase Dynamic Links 
 
-const API_KEY = 'AIzaSyCjkUcGt5h2mFyNfgJFqUXqKY1kOQnjL4g';
-const API_URL = 'https://www.googleapis.com/urlshortener/v1/url';
+// OLD API
+// const API_KEY = 'AIzaSyCjkUcGt5h2mFyNfgJFqUXqKY1kOQnjL4g';
+// const API_URL = 'https://www.googleapis.com/urlshortener/v1/url';
+
+// NEW API
+const API_KEY = 'AIzaSyASAQzCena-R-4DQUwDIosyLeNl68C7p0k';
+const API_URL = 'https://firebasedynamiclinks.googleapis.com/v1/shortLinks';
+
 let TAB_URL = '';
 let TITLE = '';
 
@@ -26,9 +33,13 @@ function onError(error) {
 }
 
 function shortenLink(link, title) {
-  const basename = "https://www.googleapis.com";
-  const urlfrag = "/urlshortener/v1/url?key=" + API_KEY;
-  const longUrl = encodeURIComponent(link);
+  // const basename = "https://www.googleapis.com";
+  // const urlfrag = "/urlshortener/v1/url?key=" + API_KEY;
+  const basename = "https://firebasedynamiclinks.googleapis.com";
+  const urlfrag = "/v1/shortLinks?key=" + API_KEY;
+  // const longUrl = encodeURIComponent(link);
+  const longDynamicLink = link;
+  const dynamicLinkDomain = 'dh3p7.app.goo.gl';
   const xhr = new XMLHttpRequest();
 
   xhr.open("POST", basename + urlfrag, true);
@@ -36,9 +47,19 @@ function shortenLink(link, title) {
   xhr.onreadystatechange = function () {
     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
       const response = (JSON.parse(xhr.responseText));
-      setURLshorten(response.id, title);
+      // setURLshorten(response.id, title);
+      setURLshorten(response.shortLink, title);
     }
   };
 
-  xhr.send(JSON.stringify({ "longUrl": link }));
+  // xhr.send(JSON.stringify({ "longUrl": link }));
+  xhr.send(JSON.stringify({
+    "dynamicLinkInfo": {
+      "dynamicLinkDomain": dynamicLinkDomain,
+      "link": link
+    },
+    "suffix": {
+      "option": "SHORT"
+    }
+  }));
 }
