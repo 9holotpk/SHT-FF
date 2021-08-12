c5168ac5374e06a39dec7f954e8c08ef7e99f9ea
Sontaya W <tonklasw@icloud.com>
2021-08-12 09:48:14 +0700

    Update 4.1

--- popup.js
+++ popup.js
@@ -1,23 +1,17 @@
 // DEV. EXTENTION BY iTON // => POPUP.JS
 // NOTE: Click to Shorten URL
-// UPDATE:  14/06/2018  - Optimize update and Bug fixed.
-//          12/06/2019  - Add QR code.
-//          13/06/2019  - Add Dark Mode.
-//          24/01/2020  - Customize #.
 
 // # Event
-document.getElementById('optionsX').addEventListener('click', show_options);
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('tag').addEventListener('click', save_optionsX);
-document.getElementById('sharebt').addEventListener('click', save_optionsX);
-document.getElementById('qrcbt').addEventListener('click', save_optionsX);
-document.getElementById('darkmode').addEventListener('click', save_optionsX);
-document.getElementById('hashtag').addEventListener('keyup', save_optionsX);
-document.getElementById('atcopy').addEventListener('click', save_optionsX);
-document.getElementById('complete').addEventListener('click', copy);
-document.getElementById('copped').addEventListener('click', copy);
-
-// document.getElementById('about').addEventListener('click', gotoAbout);
+document.getElementById("optionsX").addEventListener("click", show_options);
+document.addEventListener("DOMContentLoaded", restore_options);
+document.getElementById("tag").addEventListener("click", save_optionsX);
+document.getElementById("sharebt").addEventListener("click", save_optionsX);
+document.getElementById("qrcbt").addEventListener("click", save_optionsX);
+document.getElementById("darkmode").addEventListener("click", save_optionsX);
+document.getElementById("hashtag").addEventListener("keyup", save_optionsX);
+document.getElementById("atcopy").addEventListener("click", save_optionsX);
+document.getElementById("complete").addEventListener("click", copy);
+document.getElementById("copped").addEventListener("click", copy);
 
 // # Value
 let w_hashtags = "&hashtags=iShortener";
@@ -28,36 +22,40 @@ let share_now = false;
 onGot();
 
 function onGot() {
-  browser.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs) {
-    TAB_URL = tabs[0].url;
-    TITLE = tabs[0].title;
-    // console.log('Page: (' + tabs[0].id + ') ' + TITLE);
-    if (TAB_URL) {
-      let URL_RES = TAB_URL.substring(0, 4);
-      if (URL_RES === 'http') {
-        browser.runtime.sendMessage({ script: "shortenLink", tab_url: TAB_URL, title: TITLE });
-      } else {
-        document.getElementById("loading").style.display = "none";
-        document.getElementById("faq").style.display = "inline";
-        document.getElementById("noURL").style.display = "block";
-        document.getElementById("shareY").style.display = "none";
-        document.getElementById("qrcX").style.display = "none";
+  browser.tabs.query(
+    { active: true, lastFocusedWindow: true },
+    function (tabs) {
+      TAB_URL = tabs[0].url;
+      TITLE = tabs[0].title;
+      if (TAB_URL) {
+        let URL_RES = TAB_URL.substring(0, 4);
+        if (URL_RES === "http") {
+          browser.runtime.sendMessage({
+            script: "shortenLink",
+            tab_url: TAB_URL,
+            title: TITLE,
+          });
+        } else {
+          document.getElementById("loading").style.display = "none";
+          document.getElementById("faq").style.display = "inline";
+          document.getElementById("noURL").style.display = "block";
+          document.getElementById("shareY").style.display = "none";
+          document.getElementById("qrcX").style.display = "none";
+        }
       }
     }
-  });
+  );
 }
 
-browser.runtime.onMessage.addListener(
-  function (request) {
-    let resultSht = request;
-    if (resultSht.shortLink) {
-      setURLshorten(resultSht.shortLink, resultSht.title, resultSht.longLink);
-    }
+browser.runtime.onMessage.addListener(function (request) {
+  let resultSht = request;
+  if (resultSht.shortLink) {
+    setURLshorten(resultSht.shortLink, resultSht.title, resultSht.longLink);
   }
-);
+});
 
 function checkDNT() {
-  console.log('doNotTrack', window.navigator.doNotTrack);
+  console.log("doNotTrack", window.navigator.doNotTrack);
   let dnt = window.navigator.doNotTrack;
   if (dnt == "1") {
     document.getElementById("dnt").style.display = "block";
@@ -70,21 +68,27 @@ function checkDNT() {
 
 function restore_options() {
   let manifestData = browser.runtime.getManifest();
-  let version = document.getElementById('version');
-  let getting = browser.storage.local.get(["twitterTag", "sharebutton", "qrcode", "mode", "hashtag", "autocopy"]);
+  let version = document.getElementById("version");
+  let getting = browser.storage.local.get([
+    "twitterTag",
+    "sharebutton",
+    "qrcode",
+    "mode",
+    "hashtag",
+    "autocopy",
+  ]);
 
   getting.then(onGotX, onError);
   version.textContent = manifestData.version;
-
 }
 
 function onGotX(items) {
-  let tag = document.getElementById('tag');
-  let sharebt = document.getElementById('sharebt');
-  let qrcbt = document.getElementById('qrcbt');
-  let darkbt = document.getElementById('darkmode');
-  let hashtag = document.getElementById('hashtag');
-  let atcopy = document.getElementById('atcopy');
+  let tag = document.getElementById("tag");
+  let sharebt = document.getElementById("sharebt");
+  let qrcbt = document.getElementById("qrcbt");
+  let darkbt = document.getElementById("darkmode");
+  let hashtag = document.getElementById("hashtag");
+  let atcopy = document.getElementById("atcopy");
 
   if (items.twitterTag) {
     tag.checked = items.twitterTag.value;
@@ -93,7 +97,7 @@ function onGotX(items) {
       hashtag.disabled = false;
       hashtag.value = items.hashtag.value;
     } else {
-      w_hashtags = '';
+      w_hashtags = "";
       hashtag.disabled = true;
     }
   } else {
@@ -116,7 +120,7 @@ function onGotX(items) {
 
   if (items.qrcode) {
     qrcbt.checked = items.qrcode.value;
-    let show_qrc = document.getElementById('qrcX');
+    let show_qrc = document.getElementById("qrcX");
     if (items.qrcode.value) {
       show_qrc.style.display = "block";
     } else {
@@ -129,9 +133,9 @@ function onGotX(items) {
   if (items.mode) {
     darkbt.checked = items.mode.value;
     if (items.mode.value) {
-      document.getElementById("theme").classList.add('darkmode');
+      document.getElementById("theme").classList.add("darkmode");
     } else {
-      document.getElementById("theme").classList.remove('darkmode');
+      document.getElementById("theme").classList.remove("darkmode");
     }
   } else {
     darkbt.checked = false;
@@ -165,7 +169,7 @@ function setURLshorten(shtURL, title, LgURL) {
     }
     share(shtURL, title, LgURL);
     genQRC(shtURL);
-    input.blur()
+    input.blur();
   }
 }
 
@@ -200,16 +204,20 @@ function hide() {
 }
 
 function share(shtURL, title_o, lgURL) {
-  let title = encodeURI(title_o)
+  let title = encodeURI(title_o);
   let url = encodeURI(shtURL);
   let tweetbt = document.getElementById("tweetbt");
   let facebookbt = document.getElementById("facebookbt");
   let hashtags = w_hashtags;
 
-  tweetbt.href = "https://twitter.com/intent/tweet?size=m&url=" + shtURL + "&related=9holotpk&text=" + title + hashtags;
+  tweetbt.href =
+    "https://twitter.com/intent/tweet?size=m&url=" +
+    shtURL +
+    "&related=9holotpk&text=" +
+    title +
+    hashtags;
 
   facebookbt.href = "https://www.facebook.com/sharer/sharer.php?u=" + lgURL;
-
 }
 
 function show_options() {
@@ -223,24 +231,23 @@ function show_options() {
 
 function gotoAbout() {
   window.open(
-    'about:addons',
-    '_blank' // <- This is what makes it open in a new window.
+    "about:addons",
+    "_blank" // <- This is what makes it open in a new window.
   );
-
 }
 
 function save_optionsX() {
-  let tag = document.getElementById('tag').checked;
-  let sharebt = document.getElementById('sharebt').checked;
-  let show_button = document.getElementById('shareY');
-  let qrcbt = document.getElementById('qrcbt').checked;
-  let show_qrc = document.getElementById('qrcX');
-  let darkbt = document.getElementById('darkmode').checked;
-  let hashtag_in = document.getElementById('hashtag');
-  let atcopy = document.getElementById('atcopy').checked;
+  let tag = document.getElementById("tag").checked;
+  let sharebt = document.getElementById("sharebt").checked;
+  let show_button = document.getElementById("shareY");
+  let qrcbt = document.getElementById("qrcbt").checked;
+  let show_qrc = document.getElementById("qrcX");
+  let darkbt = document.getElementById("darkmode").checked;
+  let hashtag_in = document.getElementById("hashtag");
+  let atcopy = document.getElementById("atcopy").checked;
 
   if (!tag) {
-    hashtag_in.value = "iShortener"
+    hashtag_in.value = "iShortener";
     hashtag_in.disabled = true;
   } else {
     hashtag_in.disabled = false;
@@ -253,9 +260,9 @@ function save_optionsX() {
   }
 
   if (darkbt) {
-    document.getElementById("theme").classList.add('darkmode');
+    document.getElementById("theme").classList.add("darkmode");
   } else {
-    document.getElementById("theme").classList.remove('darkmode');
+    document.getElementById("theme").classList.remove("darkmode");
   }
 
   if (sharebt) {
@@ -267,33 +274,33 @@ function save_optionsX() {
   var twitterTag = {
     name: "#",
     value: tag,
-  }
+  };
   var hashtag = {
     name: "Hashtag",
     value: hashtag_in.value,
-  }
+  };
   var sharebutton = {
     name: "Facebook, Twitter",
     value: sharebt,
-  }
+  };
   var qrcode = {
     name: "QR Code",
     value: qrcbt,
-  }
+  };
 
   var mode = {
-    name: 'Mode',
-    value: darkbt
-  }
+    name: "Mode",
+    value: darkbt,
+  };
 
   var autocopy = {
-    name: 'autocopy',
-    value: atcopy
-  }
+    name: "autocopy",
+    value: atcopy,
+  };
 
-  console.log(mode);
   // store the objects
-  browser.storage.local.set({ twitterTag, sharebutton, qrcode, mode, hashtag, autocopy })
+  browser.storage.local
+    .set({ twitterTag, sharebutton, qrcode, mode, hashtag, autocopy })
     .then(setItem, onError);
 }
 
@@ -304,6 +311,3 @@ function setItem() {
 function onError(error) {
   console.log(`Error: ${error}`);
 }
-
-
-
